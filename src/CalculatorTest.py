0f2478704eddd7f7e00dd427af7b3cf241143112
khangtran <kt36@njit.edu>
2020-10-28 14:12:42 -0400

    Fearture: Test Instantiation

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -7,6 +7,8 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
 
 
 if __name__ == '__main__':
