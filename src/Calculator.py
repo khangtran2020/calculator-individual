89772d8cbdaba2974524c24b8b0264a6f86a8045
khangtran <kt36@njit.edu>
2020-10-28 14:37:35 -0400

    Feature: add Subtraction and Test Subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,12 +3,21 @@ def addition(a,b):
     b = float(b)
     return a+b
 
+def subtraction(a,b):
+    a = float(a)
+    b = float(b)
+    return a - b
+
 class Calculator:
     result = 0
 
     def add(self, a, b):
         self.result = addition(a,b)
         return addition(a,b)
+
+    def subtract(self, a, b):
+        self.result = subtraction(b,a)
+        return subtraction(b,a)
 
     def __init__(self):
         pass
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -17,5 +17,13 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
+    def test_subtraction(self):
+        test_data = CsvReader("data/Unit Test Subtraction.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
+
+
 if __name__ == '__main__':
     unittest.main()
