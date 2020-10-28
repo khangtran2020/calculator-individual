828bd0b06765dd5e31d8410df4c69ffbd5724566
khangtran <kt36@njit.edu>
2020-10-28 14:34:46 -0400

    Feature: add Addition and Test Addition

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,6 +1,6 @@
 import unittest
 from Calculator import Calculator
-
+from CsvReader import CsvReader
 
 class MyTestCase(unittest.TestCase):
 
@@ -10,6 +10,12 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
+    def test_addition(self):
+        test_data = CsvReader("data/Unit Test Addition.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
 
 if __name__ == '__main__':
     unittest.main()
